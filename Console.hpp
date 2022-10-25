ad12497fea4a433fe721843d0791eb62e72bc4d5
CodeMouse179 <codemouse179@gmail.com>
2022-10-25 22:02:25 +0300

    Update Console.hpp

--- Console.hpp
+++ Console.hpp
@@ -1,4 +1,8 @@
-﻿#ifndef SYSTEM_CONSOLE_HPP
+﻿//Modified date : 2022/10/25
+//Designer : CodeMouse179(编程鼠鼠)
+//License : MIT
+
+#ifndef SYSTEM_CONSOLE_HPP
 #define SYSTEM_CONSOLE_HPP
 
 namespace System
