e80f90caea95acbdff24e86678a3022332b11339
CodeMouse179 <codemouse179@gmail.com>
2022-10-26 23:09:34 +0300

    release 0.1.0

--- Console.hpp
+++ Console.hpp
@@ -1,6 +1,16 @@
-﻿//Modified date : 2022/10/25
-//Designer : CodeMouse179(编程鼠鼠)
-//License : MIT
+﻿//     +--------------------------------------------------------------------------------+
+//     |                                  Console v0.1.0                                |
+//     |  Introduction : System.Console in C++                                          |
+//     |  Modified date : 2022/10/26                                                    |
+//     |  License : MIT                                                                 |
+//     |  Source code : https://github.com/CodeMouse179/Console                         |
+//     |  Doc : NULL                                                                    |
+//     |                                                                                |
+//     |                             Designer : CodeMouse179                            |
+//     |  Email : codemouse179@gmail.com                                                |
+//     |  Github : https://github.com/CodeMouse179                                      |
+//     |  Bilibili : https://space.bilibili.com/3461577785215838                        |
+//     +--------------------------------------------------------------------------------+
 
 #ifndef SYSTEM_CONSOLE_HPP
 #define SYSTEM_CONSOLE_HPP
